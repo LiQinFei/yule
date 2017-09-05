667be08d9875e66aec0c7abca076a6d67d5edd5d
LiQinFei <526700468@qq.com>
2017-09-05 22:20:58 +0800

    9yue5晚

--- yule/src/App.vue
+++ yule/src/App.vue
@@ -16,7 +16,8 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  
+  height: 100%;
+  width: 100%;
   }
 
   .heards{
--- yule/src/components/index.vue
+++ yule/src/components/index.vue
@@ -2,37 +2,69 @@
  
  <div class="box">  
    
-   <mt-header class="heards" title="大秦之子">
-      <mt-button icon="more" slot="right"></mt-button>
-   </mt-header>
+ 
     <!-- tabcontainer -->  
-      <mt-tab-container v-model="selected">
+      <mt-tab-container v-model="selected" class="boxs">
         <mt-tab-container-item id="jiaoliu" class="box1">
-      
+          <!-- <mt-header class="heards" fixed title="大秦之子">
+      <mt-button icon="more" slot="right"></mt-button>
+        </mt-header> -->
       <!-- 聊天 -->
             <ul class="list">
               <li>
                 <img src="../assets/liaotian.jpg" alt="">
-                <h4>一起来吐槽</h4>
+                <h4>万人吐槽</h4>
               </li>
               <li>
-                <img src="../assets//fenxiang.jpg" alt="">
+                <img src="../assets/fenxiang.jpg" alt="">
                 <h4>分享留言</h4>
               </li>
             
             </ul>
+              <img class="box1bg" src="../assets/hei.jpg" alt="">
 
-
-            <mt-cell title="一起来吐槽" label="万人同时在线聊天，快乐无极限" is-link></mt-cell>
-            <mt-cell title="分享留言" label="留下自己的脚步，分享自己的快乐" is-link></mt-cell>
+            <!-- <mt-cell title="一起来吐槽" label="万人同时在线聊天，快乐无极限" is-link></mt-cell>
+            <mt-cell title="分享留言" label="留下自己的脚步，分享自己的快乐" is-link></mt-cell> -->
           
         </mt-tab-container-item>
 
         <mt-tab-container-item id="kuaile" class="box2">
           <!-- 娱乐 -->
-              <mt-cell title="大秦音乐" label="百万音乐等你听" is-link></mt-cell>
+              <!-- <mt-cell title="大秦音乐" label="百万音乐等你听" is-link></mt-cell>
               <mt-cell title="藏头诗生成" label="百万音乐等你听" is-link></mt-cell>
-              <mt-cell title="星座运势" label="百万音乐等你听" is-link></mt-cell>
+              <mt-cell title="星座运势" label="百万音乐等你听" is-link></mt-cell> -->
+            <img class="box2bg" src="../assets/huoche.jpg" alt="">
+            <div class="sli">
+              <div class="slis">
+                  <ul class="slis1">
+                    <li>大秦音乐</li>
+                    <li>藏头诗生成</li>
+                    <li>星座运势</li>
+                    <li>你好</li>
+                    
+                  </ul>
+                  <ul class="slis2">
+                 
+                      <li>你好</li>
+                    <li>你好</li>
+                    <li>你好</li>
+                    <li>你好</li>
+                    <li>你好</li>
+                    <li>你好</li>
+                    <li>你好</li>
+                    <li>你好</li>
+                    <li>你好</li><li>你好</li>
+                    <li>你好</li>
+                    <li>你好</li>
+                    <li>你好</li><li>你好</li>
+                    <li>你好</li>
+                    <li>你好</li>
+                  
+                  </ul>
+              </div>
+
+            </div>
+
         </mt-tab-container-item>
          <mt-tab-container-item class="box3" id="users">
            <!-- 我的 --> 
@@ -201,6 +233,11 @@ export default {
   width: 100%;
   height: 100%;
 }
+.boxs{
+   width: 100%;
+  height: 100%;
+}
+
 .header_src{
   width: 100%;
   text-align: center;
@@ -232,28 +269,96 @@ export default {
   box-sizing: border-box;
 }
 
+.box2{
+  width: 100vw;
+  height: 100vh;
+  .box2bg{
+      width: 100%;
+      height: 100%;
+  }
+  .sli{
+    width: 100%;
+    height: 10rem;
+  
+    position: absolute;
+    top: 60%;
+    .slis{
+      height: 80%;
+      overflow-x: auto;
+    
+      .slis1{
+        height: 4rem;
+        white-space:nowrap;
+        
+        li{
+          display: inline-block;
+          height: 80%;
+          box-sizing: border-box;
+          padding: 1rem;
+          border: 1px solid #dddddd;
+          border-radius: 30px;
+          margin-left: 30px;
+          &:first-child{
+            margin-left: 20px;
+          }
+          background: #ffffff;
+        }
+      }
+       .slis2{
+        height: 4rem;
+          white-space:nowrap;
+         
+        li{
+          box-sizing: border-box;
+          padding: 1rem;
+          display: inline-block;
+          height: 80%;
+          border: 1px solid #dddddd;
+          border-radius: 30px;
+          margin-left: 30px;
+        background: #ffffff;
+             &:first-child{
+            margin-left: 50px;
+          }
+        }
+      }
+    }
+  
+  }
+}
+
 .box1{
   width: 100%;
   box-sizing: border-box;
+  height: 100vh;
+  //  padding-top: 40px;
+  .box1bg{
+    width: 100%;
+    height: 100%;
+  }
   .list{
     width: 100%;
-    display: flex;
+  
     padding: 2px;
-  box-sizing: border-box;
-    
+    box-sizing: border-box;
+    text-align: center;
+    position: absolute;
     li{
+     display: inline-block;
       padding: 2px;
       width: 50%;
       position: relative;
+      margin-top: 40px;
       h4{
         position: absolute;
         left: 50%;
         top: 50%;
         transform: translate(-50%,-50%);
         color: #ffffff;
+        font-size: 20px;
       }
       img{
-      
+       border-radius: 50%;
         width: 100%;
       }
     }
