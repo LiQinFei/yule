4b2c9594f724681449c76474bc2a27cc27717459
LiQinFei <526700468@qq.com>
2017-09-09 17:41:44 +0800

    6zui

--- yule/src/components/index.vue
+++ yule/src/components/index.vue
@@ -34,6 +34,10 @@
               <mt-cell title="藏头诗生成" label="百万音乐等你听" is-link></mt-cell>
               <mt-cell title="星座运势" label="百万音乐等你听" is-link></mt-cell> -->
             <img class="box2bg" src="../assets/huoche.jpg" alt="">
+        
+             <div class="clocks">
+              <canvas id="canvas" width="250" height="250"></canvas>
+            </div>
             <div class="sli">
               <div class="slis">
                   <ul class="slis1">
@@ -79,16 +83,16 @@
   
     <mt-tabbar v-model="selected" fixed>  
       <mt-tab-item id="jiaoliu">  
-        <img slot="icon" src="../assets/cry.png">  
+        <img slot="icon" src="../assets/jiaoliu.png">  
         交流  
       </mt-tab-item>  
     
       <mt-tab-item id="kuaile">  
-        <img slot="icon" src="../assets/cry.png">  
+        <img slot="icon" src="../assets/yule.png">  
         娱乐生活 
       </mt-tab-item>  
       <mt-tab-item id="users" @click.native="denglu()">  
-        <img slot="icon" src="../assets/cry.png">  
+        <img slot="icon" src="../assets/wode.png">  
         我的  
       </mt-tab-item>  
     </mt-tabbar>  
@@ -143,7 +147,107 @@ export default {
       this.selected = 'jiaoliu'
   }
   ,mounted(){
-     
+      // inner variables
+
+  canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+
+    // var width = canvas.width;
+    // var height = canvas.height;
+
+
+
+
+    setInterval(drawScene, 1000); // loop drawScene
+
+
+
+var canvas, ctx;
+var clockRadius = 100;
+
+
+// draw functions :
+function clear() { // clear canvas function
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
+
+function drawScene() { // main drawScene function
+    clear(); // clear canvas
+
+    // get current time
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    hours = hours > 12 ? hours - 12 : hours;
+    var hour = hours + minutes / 60;
+    var minute = minutes + seconds / 60;
+
+    // save current context
+    ctx.save();
+
+    // draw clock image (as background)
+
+
+    ctx.translate(canvas.width / 2, canvas.height / 2);
+    ctx.beginPath();
+
+    // draw numbers
+    ctx.font = '16px Arial';
+    ctx.fillStyle = '#fff';
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    for (var n = 1; n <= 12; n++) {
+        var theta = (n - 3) * (Math.PI * 2) / 12;
+        var x = clockRadius * 1 * Math.cos(theta);
+        var y = clockRadius * 1 * Math.sin(theta);
+        ctx.fillText(n, x, y);
+    }
+
+    // draw hour
+    ctx.save();
+    var theta = (hour - 3) * 2 * Math.PI / 12;
+    ctx.rotate(theta);
+    ctx.beginPath();
+    ctx.moveTo(-15, -5);
+    ctx.lineTo(-15, 5);
+    ctx.lineTo(clockRadius * 0.5, 1);
+    ctx.lineTo(clockRadius * 0.5, -1);
+    ctx.fill();
+    ctx.restore();
+
+    // draw minute
+    ctx.save();
+    var theta = (minute - 15) * 2 * Math.PI / 60;
+    ctx.rotate(theta);
+    ctx.beginPath();
+    ctx.moveTo(-15, -4);
+    ctx.lineTo(-15, 4);
+    ctx.lineTo(clockRadius * 0.8, 1);
+    ctx.lineTo(clockRadius * 0.8, -1);
+    ctx.fill();
+    ctx.restore();
+
+    // draw second
+    ctx.save();
+    var theta = (seconds - 15) * 2 * Math.PI / 60;
+    ctx.rotate(theta);
+    ctx.beginPath();
+    ctx.moveTo(-15, -3);
+    ctx.lineTo(-15, 3);
+    ctx.lineTo(clockRadius * 0.9, 1);
+    ctx.lineTo(clockRadius * 0.9, -1);
+    ctx.fillStyle = '#0f0';
+    ctx.fill();
+    ctx.restore();
+
+    ctx.restore();
+}
+
+// initialization
+
+  
+
   },methods:{
     getUser(){
           
@@ -291,6 +395,14 @@ export default {
 .box2{
   width: 100vw;
   height: 100vh;
+.clocks{
+  position: absolute;
+  top: 50px;
+  left: 0;
+  width: 100%;
+  text-align: center;
+}
+
   .box2bg{
       width: 100%;
       height: 100%;
